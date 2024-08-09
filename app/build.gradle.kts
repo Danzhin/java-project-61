80f5cc0a660fe1df26c7e38e4f1a6d82f1e679d5
daniil <dronzhin123@gmail.com>
2024-08-09 11:48:27 +0300

    2 stage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,25 @@
 plugins {
-    id("java")
+    java
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
